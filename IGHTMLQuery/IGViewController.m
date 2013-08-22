1b38a15bc59c9c7676f6275e228d856d69c548e0
Francis Chong <francis@ignition.hk>
2013-08-22 21:52:51 +0800

    add more to sample

--- IGHTMLQuery/IGViewController.m
+++ IGHTMLQuery/IGViewController.m
@@ -19,17 +19,16 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    
-    IGHTMLDocument* doc = [[IGHTMLDocument alloc] initWithHTMLString:@"<html><p>Hello World</p></html>" error:nil];
-    NSLog(@"doc = %@", doc.xml);
+
     for (int i=0; i < 100; i++) {
+        IGHTMLDocument* doc = [[IGHTMLDocument alloc] initWithHTMLString:@"<html><p>Hello World</p></html>" error:nil];
         doc.query(@"p")
             .firstObject
-            .append([NSString stringWithFormat:@"<p>%d</p>", i])
+            .append([NSString stringWithFormat:@"<div><span>%d</span></div>", i])
             .before(@"<a>opps</a>");
+        [doc.query(@"a") remove];
+        [doc.query(@"p") empty];
     }
-    NSLog(@"doc = %@", doc.xml);
-
 }
 
 - (void)didReceiveMemoryWarning
