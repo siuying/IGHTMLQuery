cf12d630a21b5fcaa81a784308beae1de27568a3
Francis Chong <francis@ignition.hk>
2013-08-23 01:20:38 +0800

    add more sample code

--- IGHTMLQuery/IGViewController.m
+++ IGHTMLQuery/IGViewController.m
@@ -20,14 +20,23 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 
-    for (int i=0; i < 100; i++) {
-        IGHTMLDocument* doc = [[IGHTMLDocument alloc] initWithHTMLString:@"<html><p>Hello World</p></html>" error:nil];
-        doc.query(@"p")
-            .firstObject
-            .append([NSString stringWithFormat:@"<div><span>%d</span></div>", i])
-            .before(@"<a>opps</a>");
-        [doc.query(@"a") remove];
-        [doc.query(@"p") empty];
+    for (int i=0; i < 10; i++) {
+        @autoreleasepool {
+            IGHTMLDocument* doc = [[IGHTMLDocument alloc] initWithHTMLString:@"<html><p>Hello World</p></html>" error:nil];
+            doc.query(@"p")
+                .firstObject
+                .append([NSString stringWithFormat:@"<div><span>%d</span></div>", i])
+                .before(@"<a>opps</a>");
+            doc.query(@"p")
+                .firstObject
+                .prepend([NSString stringWithFormat:@"<div><p>%d</p></div>", i])
+                .after(@"<a>woo</a>");
+            [doc.query(@"a") remove];
+            [doc.query(@"p").firstObject empty];
+            [doc.query(@"p").firstObject remove];
+            [doc.query(@"p").firstObject remove];
+            [doc.query(@"p").firstObject remove];
+        }
     }
 }
 
