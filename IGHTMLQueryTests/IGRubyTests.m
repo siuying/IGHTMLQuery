0dfbae5680f2392ca7bf9ca74ceb617aaa2574f8
Francis Chong <francis@ignition.hk>
2013-12-31 17:48:48 +0800

    cleanup text

--- IGHTMLQueryTests/IGRubyTests.m
+++ IGHTMLQueryTests/IGRubyTests.m
@@ -16,6 +16,7 @@
 {
     IGXMLDocument* doc;
     JSContext* context;
+    JSValue* instanceEval;
 }
 @end
 
@@ -37,68 +38,57 @@
     
     // setup context
     context[@"doc"] = doc;
-}
-
-- (void)tearDown
-{
-    // Put teardown code here; it will be run once, after the last test case.
-    [super tearDown];
+    
+    // instance_eval on a doc
+    instanceEval = [context evaluateRuby:@"lambda { |doc, script| XMLNode.new(doc).instance_eval(&eval(\"lambda { #{script} }\")) }"];
 }
 
 - (void)testNodeCore
 {
-    JSValue* block = [context evaluateRuby:@"lambda { |doc, script| XMLNode.new(doc).instance_eval(&eval(\"lambda { #{script} }\")) }"];
-    JSValue* node = [block callWithArguments:@[doc, @"self.tag"]];
+    JSValue* node = [instanceEval callWithArguments:@[doc, @"self.tag"]];
     XCTAssertEqualObjects(@"catalog", [node toString], @"should be catalog");
 }
 
 - (void)testNodeAttribute
 {
-    JSValue* block = [context evaluateRuby:@"lambda { |doc, script| XMLNode.new(doc).instance_eval(&eval(\"lambda { #{script} }\")) }"];
-    JSValue* node = [block callWithArguments:@[doc, @"self.children.first['country'] = 'Hong Kong'; self.children.first['country'] "]];\
+    JSValue* node = [instanceEval callWithArguments:@[doc, @"self.children.first['country'] = 'Hong Kong'; self.children.first['country'] "]];\
     XCTAssertEqualObjects(@"Hong Kong", [node toString], @"should be Hong Kong");
 }
 
 - (void)testNodeQuery
 {
-    JSValue* block = [context evaluateRuby:@"lambda { |doc, script| XMLNode.new(doc).instance_eval(&eval(\"lambda { #{script} }\")) }"];
-    JSValue* node = [block callWithArguments:@[doc, @"self.xpath('//cd/price').first.text.to_f"]];
+    JSValue* node = [instanceEval callWithArguments:@[doc, @"self.xpath('//cd/price').first.text.to_f"]];
     XCTAssertEqualWithAccuracy(10.9, [[node toNumber] doubleValue], 0.001, @"should find first price");
 }
 
 - (void)testNodeManipulationRemove
 {
-    JSValue* block = [context evaluateRuby:@"lambda { |doc, script| XMLNode.new(doc).instance_eval(&eval(\"lambda { #{script} }\")) }"];
-    JSValue* node = [block callWithArguments:@[doc, @"self.xpath('//cd').each {|n| n.remove }; self.xpath('//cd').nodes "]];
+    JSValue* node = [instanceEval callWithArguments:@[doc, @"self.xpath('//cd').each {|n| n.remove }; self.xpath('//cd').nodes "]];
     XCTAssertEqual(0U, [[node toArray] count], @"should remove cds");
 }
 
 - (void)testNodeManipulationEmpty
 {
-    JSValue* block = [context evaluateRuby:@"lambda { |doc, script| XMLNode.new(doc).instance_eval(&eval(\"lambda { #{script} }\")) }"];
-    JSValue* node = [block callWithArguments:@[doc, @"self.empty; self.xml"]];
+    JSValue* node = [instanceEval callWithArguments:@[doc, @"self.empty; self.xml"]];
     XCTAssertEqualObjects(@"<catalog/>", [node toString], @"should empty xml");
 }
 
 - (void)testNodeSet
 {
-    JSValue* block = [context evaluateRuby:@"lambda { |doc, script| XMLNode.new(doc).instance_eval(&eval(\"lambda { #{script} }\")) }"];
-    JSValue* node = [block callWithArguments:@[doc, @"first = self.children.all.first; first.tag"]];
+    JSValue* node = [instanceEval callWithArguments:@[doc, @"first = self.children.all.first; first.tag"]];
     XCTAssertEqualObjects(@"cd", [node toString], @"should be cd");
     
 }
 
 - (void)testNodeSetManipulation
 {
-    JSValue* block = [context evaluateRuby:@"lambda { |doc, script| XMLNode.new(doc).instance_eval(&eval(\"lambda { #{script} }\")) }"];
-    JSValue* node = [block callWithArguments:@[doc, @"self.xpath('//cd').remove; self.xpath('//cd').nodes "]];
+    JSValue* node = [instanceEval callWithArguments:@[doc, @"self.xpath('//cd').remove; self.xpath('//cd').nodes "]];
     XCTAssertEqual(0U, [[node toArray] count], @"should remove cds");
 }
 
 - (void)testNodeSetEnumerable
 {
-    JSValue* block = [context evaluateRuby:@"lambda { |doc, script| XMLNode.new(doc).instance_eval(&eval(\"lambda { #{script} }\")) }"];
-    JSValue* node = [block callWithArguments:@[doc, @"self.xpath('//cd').find {|node| node.xpath('./price').text.to_f < 9.0 }.xpath('./title').text"]];
+    JSValue* node = [instanceEval callWithArguments:@[doc, @"self.xpath('//cd').find {|node| node.xpath('./price').text.to_f < 9.0 }.xpath('./title').text"]];
     NSString* title = [node toString];
     XCTAssertEqualObjects((@"Greatest Hits"), title, @"title should be nil");
 }
