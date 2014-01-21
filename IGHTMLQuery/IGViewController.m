8fd546049468d782a56b200dbda4366ff2bd3bfb
Francis Chong <francis@ignition.hk>
2014-01-21 14:30:13 +0800

    test for any leaks by using the library

--- IGHTMLQuery/IGViewController.m
+++ IGHTMLQuery/IGViewController.m
@@ -21,15 +21,25 @@
 {
     
     [super viewDidLoad];
-    
-    IGHTMLDocument* doc = [[IGHTMLDocument alloc] initWithHTMLString:@"<html><p>Hello World</p></html>" error:nil];
-    NSString* content = [[[doc queryWithXPath:@"//p"] firstObject] text];
-    NSLog(@"content from Objective-C = %@", content);
-    
-    JSContext *context = [[JSContext alloc] init];
-    context[@"doc"] = doc;
-    JSValue* output = [context evaluateScript:@"doc.queryWithXPath('//p').firstObject().text()"];
-    NSLog(@"content from JavaScript = %@", [output toString]);
+
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSLog(@"begin loop");
+        for (int i=0 ; i< 1000; i++) {
+            @autoreleasepool {
+                IGHTMLDocument* doc = [[IGHTMLDocument alloc] initWithHTMLString:@"<html><p>Hello World</p></html>" error:nil];
+                NSString* content = [[[doc queryWithXPath:@"//p"] firstObject] text];
+                NSLog(@"content from Objective-C = %@", content);
+                
+                JSVirtualMachine* vm = [[JSVirtualMachine alloc] init];
+                JSContext *context = [[JSContext alloc] initWithVirtualMachine:vm];
+                context[@"doc"] = doc;
+
+                JSValue* output = [context evaluateScript:@"doc.queryWithXPath('//p').first"];
+                NSLog(@"content from JavaScript = %@", [output toString]);
+            }
+        }
+        NSLog(@"end loop");
+    });
 }
 
 - (void)didReceiveMemoryWarning
